a01ec30271814108e92945e66e89511a6a861cb1
Daniel Tkach <danieltkach@gmail.com>
2024-07-06 22:23:14 -0300

    time conversion

--- Hacker Rank/timeConversion.test.ts
+++ Hacker Rank/timeConversion.test.ts
@@ -5,22 +5,22 @@ describe('timeConversion', () => {
     test('handles empty values', () => {
         expect(timeConversion("")).toEqual("");
     });
-    
-    test('handles times missing hh, mm, ss or am/pm', ()=> {
-        expect(()=> timeConversion("12am")).toThrow(ERROR_FORMAT);
-        expect(()=> timeConversion("12pm")).toThrow(ERROR_FORMAT);
-        expect(()=> timeConversion("12:00am")).toThrow(ERROR_FORMAT);
-        expect(()=> timeConversion("12:00pm")).toThrow(ERROR_FORMAT);
-        expect(()=> timeConversion("12:00:00")).toThrow(ERROR_FORMAT); 
-    });
-    
-    test('handles incorrect times with hours over 12, and minutes or seconds over 59', ()=> {
-        expect(()=> timeConversion("13:00:00pm")).toThrow(ERROR_FORMAT);
-        expect(()=> timeConversion("12:60:00pm")).toThrow(ERROR_FORMAT);
-        expect(()=> timeConversion("12:00:60pm")).toThrow(ERROR_FORMAT);
+
+    test('handles times missing hh, mm, ss or am/pm', () => {
+        expect(() => timeConversion("12am")).toThrow(ERROR_FORMAT);
+        expect(() => timeConversion("12pm")).toThrow(ERROR_FORMAT);
+        expect(() => timeConversion("12:00am")).toThrow(ERROR_FORMAT);
+        expect(() => timeConversion("12:00pm")).toThrow(ERROR_FORMAT);
+        expect(() => timeConversion("12:00:00")).toThrow(ERROR_FORMAT);
     });
 
-    test('returns the correct time', ()=> {
+    test('handles incorrect times with hours over 12, and minutes or seconds over 59', () => {
+        expect(() => timeConversion("13:00:00pm")).toThrow(ERROR_FORMAT);
+        expect(() => timeConversion("12:60:00pm")).toThrow(ERROR_FORMAT);
+        expect(() => timeConversion("12:00:60pm")).toThrow(ERROR_FORMAT);
+    });
+
+    test('returns the correct time', () => {
         expect(timeConversion("12:25:59am")).toEqual("00:25:59");
         expect(timeConversion("12:25:59pm")).toEqual("12:25:59");
         expect(timeConversion("10:09:09am")).toEqual("10:09:09");
